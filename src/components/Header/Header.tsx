2f46d17e38439f3d164b55591e94f4727ace14f8
electra777 <electra0789@gmail.com>
2023-03-26 23:02:23 +0300

    added header top

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,39 +4,43 @@ const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.top}>
-				<div className={styles.row}>
-					<div className={styles.item}>
-						<div className={styles.title}>г.Кокчетав, ул. Ж. Ташенова 129Б</div>
-						<div className={styles.text}>(Рынок Восточный)</div>
+				<div className={styles.container}>
+					<div className={styles.row}>
+						<div className={styles.item}>
+							<div className={styles.title}>г.Кокчетав, ул. Ж. Ташенова 129Б</div>
+							<div className={styles.text}>(Рынок Восточный)</div>
+						</div>
+						<div className={styles.item}>
+							<a href="mailto:opt.sultan@mail.ru" className={styles.title}>
+								opt.sultan@mail.ru
+							</a>
+							<div className={styles.text}>На связи в любое время</div>
+						</div>
 					</div>
-					<div className={styles.item}>
-						<div className={styles.title}>opt.sultan@mail.ru</div>
-						<div className={styles.text}>На связи в любое время</div>
-					</div>
-				</div>
 
-				<ul className={styles.nav}>
-					<li className={styles.navItem}>
-						<a href="#" className={styles.navItemLink}>
-							О компании
-						</a>
-					</li>
-					<li className={styles.navItem}>
-						<a href="#" className={styles.navItemLink}>
-							Доставка и оплата
-						</a>
-					</li>
-					<li className={styles.navItem}>
-						<a href="#" className={styles.navItemLink}>
-							Возврат
-						</a>
-					</li>
-					<li className={styles.navItem}>
-						<a href="#" className={styles.navItemLink}>
-							Контакты
-						</a>
-					</li>
-				</ul>
+					<ul className={styles.nav}>
+						<li className={styles.navItem}>
+							<a href="#" className={styles.navItemLink}>
+								О компании
+							</a>
+						</li>
+						<li className={styles.navItem}>
+							<a href="#" className={styles.navItemLink}>
+								Доставка и оплата
+							</a>
+						</li>
+						<li className={styles.navItem}>
+							<a href="#" className={styles.navItemLink}>
+								Возврат
+							</a>
+						</li>
+						<li className={styles.navItem}>
+							<a href="#" className={styles.navItemLink}>
+								Контакты
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</header>
 	);
