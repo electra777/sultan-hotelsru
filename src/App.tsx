59444d06ae907d6d052c9e2a4d1d25a21348a947
electra777 <electra0789@gmail.com>
2023-03-30 22:22:52 +0300

    added ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header';
 import styles from './App.module.css';
 import Footer from './components/Footer/Footer';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import ProductCard from './components/ProductCard/ProductCard';
 
 function App(): JSX.Element {
 	return (
@@ -9,6 +10,8 @@ function App(): JSX.Element {
 			<Header />
 			<div className={styles.container}>
 				<Breadcrumbs />
+
+				<ProductCard />
 			</div>
 			<Footer />
 		</div>
