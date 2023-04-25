b87137fec05505e29776cf56397ad4136359666c
electra777 <electra0789@gmail.com>
2023-04-25 20:18:20 +0300

    added styles for adding product in Admin

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,98 +1,131 @@
 import { Link } from 'react-router-dom';
 import styles from './Admin.module.css';
 import Input from '../../components/Input/Input';
+import cn from 'classnames';
+import Button from '../../components/Button/Button';
 
 const Admin = () => {
+	const arrayTypes = [
+		'Уход за телом',
+		'Уход за руками',
+		'Уход за ногами',
+		'Уход за лицом',
+		'Уход за волосами',
+		'Средства для загара',
+		'Средства для бритья',
+		'Подарочные наборы',
+		'Гигиеническая продукция',
+		'Гигиена полости рта',
+		'Бумажная продукция',
+	];
+
 	return (
 		<div className={styles.admin}>
 			<div className={styles.title}>Управление товарами</div>
 
+			<div className={styles.subTitle}>Добавление товара</div>
 			<form className={styles.addingProduct}>
-				<div className={styles.subTitle}>Добавление товара</div>
+				<div className={styles.col}>
+					<div className={styles.group}>
+						<label htmlFor="name" className={styles.label}>
+							Название*
+						</label>
+						<Input id="name" placeholder="Введите название товара" />
+					</div>
+					<div className={styles.group}>
+						<label htmlFor="price" className={styles.label}>
+							Стоимость (₸)*
+						</label>
+						<Input id="price" placeholder="Введите стоимость товара" />
+					</div>
+					<div className={styles.group}>
+						<label htmlFor="barcode" className={styles.label}>
+							Штрихкод*
+						</label>
+						<Input id="barcode" placeholder="Введите штрихкод товара" />
+					</div>
+					<div className={styles.group}>
+						<label htmlFor="manufacturer" className={styles.label}>
+							Производитель*
+						</label>
+						<Input id="manufacturer" placeholder="Введите производителя товара" />
+					</div>
+					<div className={styles.group}>
+						<label htmlFor="brand" className={styles.label}>
+							Брэнд*
+						</label>
+						<Input id="brand" placeholder="Введите брэнд товара" />
+					</div>
 
-				<div className={styles.group}>
-					<label htmlFor="name" className={styles.label}>
-						Название*
-					</label>
-					<Input id="name" placeholder="Введите название товара" />
-				</div>
-				<div className={styles.group}>
-					<label htmlFor="price" className={styles.label}>
-						Стоимость (₸)*
-					</label>
-					<Input id="price" placeholder="Введите стоимость товара" />
-				</div>
-				<div className={styles.group}>
-					<label htmlFor="barcode" className={styles.label}>
-						Штрихкод*
-					</label>
-					<Input id="barcode" placeholder="Введите штрихкод товара" />
-				</div>
-				<div className={styles.group}>
-					<label htmlFor="manufacturer" className={styles.label}>
-						Производитель*
-					</label>
-					<Input id="manufacturer" placeholder="Введите производителя товара" />
-				</div>
-				<div className={styles.group}>
-					<label htmlFor="brand" className={styles.label}>
-						Брэнд*
-					</label>
-					<Input id="brand" placeholder="Введите брэнд товара" />
-				</div>
+					<div className={styles.group}>
+						<label htmlFor="weight" className={styles.label}>
+							Вес
+						</label>
+						<Input id="weight" placeholder="Введите вес товара" />
+					</div>
+					<div className={styles.group}>
+						<label htmlFor="volume" className={styles.label}>
+							Объём
+						</label>
+						<Input id="volume" placeholder="Введите объём товара" />
+					</div>
 
-				<div className={styles.group}>
-					<label htmlFor="weight" className={styles.label}>
-						Вес
-					</label>
-					<Input id="weight" placeholder="Введите вес товара" />
-				</div>
-				<div className={styles.group}>
-					<label htmlFor="volume" className={styles.label}>
-						Объём
-					</label>
-					<Input id="volume" placeholder="Введите объём товара" />
-				</div>
-
-				<div className={styles.group}>
-					<label htmlFor="size" className={styles.label}>
-						Размер
-					</label>
-					<Input id="size" placeholder="Введите размер товара" />
-				</div>
-
-				<div className={styles.group}>
-					<label htmlFor="type" className={styles.label}>
-						Тип ухода
-					</label>
-					<div className={styles.selectWrapper}>
-						<select name="type" id="type" className={styles.select}>
-							<option value="0" selected={true}>
-								Выберите тип ухода
-							</option>
-							<option value="1">Уход за телом</option>
-							<option value="2">Уход за руками</option>
-							<option value="3">Уход за ногами</option>
-						</select>
+					<div className={styles.group}>
+						<label htmlFor="size" className={styles.label}>
+							Размер
+						</label>
+						<Input id="size" placeholder="Введите размер товара" />
 					</div>
 				</div>
 
-				<div className={styles.group}>
-					<input type="checkbox" className={styles.checkbox} />
-					<label htmlFor="inStock" className={styles.label}>
-						В наличии
-					</label>
-				</div>
+				<div className={styles.col}>
+					<div className={styles.group}>
+						<label htmlFor="type" className={styles.label}>
+							Тип ухода
+						</label>
 
-				<div className={styles.group}>
-					<label htmlFor="description" className={styles.label}>
-						Описание
-					</label>
-					<textarea
-						name="description"
-						id="description"
-						placeholder="Введите описание товара"
-						className={styles.textarea}></textarea>
+						{arrayTypes.map((item) => {
+							return (
+								<div className={styles.checkboxesWrapper}>
+									<input type="checkbox" className={styles.checkbox} />
+									<label htmlFor="inStock" className={styles.label}>
+										{item}
+									</label>
+								</div>
+							);
+						})}
+					</div>
+
+					<div className={styles.group}>
+						<label htmlFor="description" className={styles.label}>
+							Описание
+						</label>
+						<textarea
+							name="description"
+							id="description"
+							placeholder="Введите описание товара"
+							className={styles.textarea}></textarea>
+					</div>
+
+					<div className={styles.group}>
+						<label htmlFor="image" className={styles.label}>
+							Картинка
+						</label>
+						<input
+							type="file"
+							id="file"
+							accept=".png,.jpg,.jpeg,.svg"
+							className={styles.fileInput}
+						/>
+					</div>
+
+					<div className={styles.group}>
+						<input type="checkbox" className={styles.checkbox} />
+						<label htmlFor="inStock" className={styles.label}>
+							В наличии
+						</label>
+					</div>
+					<Button>Добавить товар</Button>
 				</div>
 			</form>
 		</div>
